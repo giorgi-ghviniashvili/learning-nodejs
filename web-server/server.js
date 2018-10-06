065155c918fc2f45a4ed7fcd55debd9c84278e2a
Giorgi Ghviniashvili <mr.g.ghv@gmail.com>
2018-10-07 02:42:00 +0400

    Setup start scrip and heroku port

--- web-server/server.js
+++ web-server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const hbs = require('hbs')
 
+const port = process.env.PORT || 3000
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -27,4 +28,6 @@ app.get('/', (req, res) => {
   })
 })
 
-app.listen(3000)
+app.listen(port, () => {
+  console.log(`Server is litening on port ${port}`);
+})
