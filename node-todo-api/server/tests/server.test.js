5571f944b178f89edbb660cd7911a692ab09b071
Giorgi Ghviniashvili <mr.g.ghv@gmail.com>
2018-10-13 20:37:27 +0400

    added get Todos test cases

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest')
 const {app} = require('../server')
 const {Todo} = require('../models/todo')
 
+const dummyTodos = [{
+  text: 'first test todo'
+}, {
+  text: 'second test todo'
+}]
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done())
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(dummyTodos)
+  }).then(() => done())
 })
 
 describe('Post /todos', () => {
@@ -24,7 +32,7 @@ describe('Post /todos', () => {
           return done(err)
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1)
           expect(todos[0].text).toBe(text)
           done()
@@ -47,9 +55,22 @@ describe('Post /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0)
+          expect(todos.length).toBe(dummyTodos.length)
           done()
         }).catch(e => done(e))
       })
+  })
+})
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+
+    request(app)
+      .get('/api/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(dummyTodos.length)
+      })
+      .end(done)
   })
 })
