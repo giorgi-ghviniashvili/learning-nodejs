7696190f61b81610f8fa9b1d5253046e24ceeb3d
Giorgi Ghviniashvili <mr.g.ghv@gmail.com>
2018-10-15 23:17:31 +0400

    added config

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,3 +1,6 @@
+require('./config/config')
+
+var _ = require('lodash')
 var express = require('express')
 var bodyParser = require('body-parser')
 var {ObjectID} = require('mongodb')
@@ -7,9 +10,15 @@ var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
 
 var app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send({
+    app: 'App under construction'
+  })
+})
 
 app.post('/api/todos', (req, res) => {
   var todo = new Todo({
@@ -49,6 +58,53 @@ app.get('/api/todos/:id', (req, res) => {
   }).catch((e) => {
     res.status(400).send()
   })
+})
+
+app.delete('/api/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+
+    res.send({todo})
+  }, (err) => {
+    return res.status(404).send()
+  }).catch((e) => {
+    res.status(400).send()
+  })
+})
+
+app.patch('/api/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed'])
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime()
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+
+    res.send({todo})
+  }).catch((err) => {
+    res.status(400).send()
+  })
+
 })
 
 app.listen(port, () => {
--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -103,3 +103,62 @@ describe('GET /api/todos/:id', () => {
       .end(done)
   })
 })
+
+describe('DELETE /api/todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = dummyTodos[1]._id.toHexString()
+
+    request(app)
+      .delete(`/api/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId)
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toBe(null)
+          done()
+        }).catch(e => {
+          return done(e)
+        })
+      })
+  })
+
+  it('should return 404 if todo not found', (done) => {
+    request(app)
+      .delete(`/api/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done)
+  })
+
+  it('shoudl return 404 if object id is invalid', (done) => {
+    request(app)
+      .delete(`/api/todos/123`)
+      .expect(404)
+      .end(done)
+  })
+})
+
+describe('/PATCH /api/todo/:id', () => {
+  it('should update todo', (done) => {
+    var hexId = dummyTodos[0]._id.toHexString()
+    var text = 'this should be a new text'
+
+    request(app)
+      .patch(`/api/todos/${hexId}`)
+      .send({
+        completed: true,
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text)
+        expect(res.body.todo.completed).toBe(true)
+      })
+      .end(done)
+  })
+})
