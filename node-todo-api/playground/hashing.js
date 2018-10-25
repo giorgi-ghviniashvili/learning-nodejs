f6091a379931e265f313cddbfcae4a938362f097
giorgi-ghviniashvili <mr.g.ghv@gmail.com>
2018-10-25 16:27:54 +0400

    hashing the password

--- node-todo-api/playground/hashing.js
+++ node-todo-api/playground/hashing.js
@@ -1,18 +1,35 @@
 // sha 256
 // const {SHA256} = require('crypto-js')
+// const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+var password = 'abc123!'
+
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+    
+//   })
+// })
+
+var hashedPassword = '$2a$10$oqNsEZymYpHUz7ZUWbasmeZ/WkIrcTQb8dKzF9sUNnBtrrKRaQx7y';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
 
 // var message = 'Hey, I am an new user.'
 // var hash = SHA256(message).toString()
 
 // console.log(hash);
-const jwt = require('jsonwebtoken')
 
-var data = {
-  id: 10
-}
+// var data = {
+//   id: 10
+// }
 
-var token = jwt.sign(data, '123abc')
-console.log(token)
+// var token = jwt.sign(data, '123abc')
+// console.log(token)
 
-var varify = jwt.verify(token, '123abc')
-console.log(varify);
+// var varify = jwt.verify(token, '123abc')
+// console.log(varify);
--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const jwt = require('jsonwebtoken')
 const _ = require('lodash')
+const bcrypt = require('bcryptjs')
 
 var UserScheme = new mongoose.Schema({
   email: {
@@ -66,6 +67,23 @@ UserScheme.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   })
 }
+
+UserScheme.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    var password = user.password;
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      })
+    })
+  } else {
+    next();
+  }
+})
 
 var User = mongoose.model('User', UserScheme)
 
